74aaf3bc2e5285e5c41e8d95e6b7f14b20a8518c
craig <166697115+caigyyy@users.noreply.github.com>
2025-09-16 14:34:44 +0100

    Create DataStructures.py

---
+++ DataStructures.py
@@ -0,0 +1,12 @@
+# Create a list of integers
+numbers = [12, 5, 8, 22, 7, 19, 30, 4, 10, 16]
+
+# Find largest, smallest, and average
+print("Largest:",  max(numbers))
+print("Smallest:", min(numbers))
+print("Average:", sum(numbers) / len(numbers))
+
+# Convert to set
+num_set = set(numbers)
+print("Original list:", numbers)
+print("Converted set:", num_set # Remove duplicates, unordered
